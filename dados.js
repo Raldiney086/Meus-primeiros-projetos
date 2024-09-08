06c258c666c39161586096b325469c4bcc34c55b
Raldiney086 <raldineyjose3@gmail.com>
2024-09-08 13:25:26 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,27 @@
+let dados = [
+    {
+    titulo: "Ronie Colermam",
+    descricao: "É uma lenda do fisculturismo, conhecido por sua força incrível. Conquistando o Mr. Olimpia oito vezes consecutivas.",
+    link: "https://www.bing.com/ck/a?!&&p=0461a2b045a0f5e7JmltdHM9MTcyNTMyMTYwMCZpZ3VpZD0xZTU3MWZlMy01ODU3LTY4NjAtMTM2OC0wZTdkNTkzYzY5MzYmaW5zaWQ9NTI0Nw&ptn=3&ver=2&hsh=3&fclid=1e571fe3-5857-6860-1368-0e7d593c6936&psq=ronnie+coleman&u=a1aHR0cHM6Ly9wdC53aWtpcGVkaWEub3JnL3dpa2kvUm9ubmllX0NvbGVtYW4jOn46dGV4dD1Sb25hbGQgRGVhbiAiUm9ubmllIiBDb2xlbWFuICgxMyBkZSBNYWlvIGRlIDE5NjQsIGVt&ntb=1",
+    tags: "lenda, força"
+},
+{
+    titulo: "Ranon Dino",
+    descricao: "Ranon Dino, o Dinossauro Acreano, é um fisicuturista brasileiro que conquistou o mundo com seu talento. Ele já coleciona títulos e inspira milhões de jovens.",
+    link: "https://pt.wikipedia.org/wiki/Ramon_Dino",
+    tags: "Dino, talento"
+},
+{
+    titulo: "Arnold Schwarzenegger",
+    descricao: "Considerado um dos maiores fisiculturistas de todos os tempos, ele tambêm se destacou como ator e político.",
+    link: "https://pt.wikipedia.org/wiki/Arnold_Schwarzenegger",
+    tags: "político, ator"
+},
+{
+    titulo: "Phil Heath",
+    descricao: "Com sete títulos no Mr. Olympia, Phil é um dos fisiculturistas mais domninantes da sociedade",
+    link: "https://pt.wikipedia.org/wiki/Phil_Heath",
+    tags: "sete, dominantes"
+}
+];
+
